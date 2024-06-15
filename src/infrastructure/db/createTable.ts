6ff577dddbcff960eefa4bba6cf433b593b4117b
Álvaro Zájara <alzca1@gmail.com>
2024-06-15 18:42:46 +0200

    fixed transaction

--- src/infrastructure/db/createTable.ts
+++ src/infrastructure/db/createTable.ts
@@ -7,14 +7,14 @@ const createTable = async () => {
     await client.query("BEGIN");
 
     const createTableQuery = `
-        CREATE TABLE IF NOT EXISTS todos(
-            id SERIAL PRIMARY KEY
-            title VARCHAR(255) NOT NULL
-            completed BOOLEAN NOT NULL DEFAULT FALSE
-            dateCreated TIMESTAMP NOT NULL DEFAULT NOW()
-            dateCompleted TIMESTAMP
-        )
-        `;
+    CREATE TABLE IF NOT EXISTS todos (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        completed BOOLEAN NOT NULL DEFAULT FALSE,
+        dateCreated TIMESTAMP NOT NULL DEFAULT NOW(),
+        dateCompleted TIMESTAMP
+    )
+    `;
     await client.query(createTableQuery);
     await client.query("COMMIT");
     console.info("Table 'todos' created successfully!");
