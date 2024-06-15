a38b4b0fc37d9d2d3d68899daacc995ff527d01c
Álvaro Zájara <alzca1@gmail.com>
2024-06-15 23:18:11 +0200

    added tests for todo update logic

--- tests/application/todoService.test.ts
+++ tests/application/todoService.test.ts
@@ -3,27 +3,63 @@ import app from "../../src/app";
 // import { db } from "../../src/infrastructure/db/dbConfig";
 
 describe("todoService ", () => {
-  it("create-todo POST route should return status code 403 if no title is provided", async () => {
-    const response = await request(app).post("/create-todo").send({});
-    expect(response.status).toBe(403);
-    expect(response.body).toEqual("A title must be provided in order to save a todo!");
+  describe("create-todo POST", () => {
+    it("create-todo POST route should return status code 403 if no title is provided", async () => {
+      const response = await request(app).post("/create-todo").send({});
+      expect(response.status).toBe(403);
+      expect(response.body).toEqual("A title must be provided in order to save a todo!");
+    });
+
+    it("create-todo POST route should return status code 403 if '' is provided as a title", async () => {
+      const response = await request(app).post("/create-todo").send({ title: "" });
+      expect(response.status).toBe(403);
+      expect(response.body).toEqual("A title must be provided in order to save a todo!");
+    });
+
+    it("create-todo POST route should return status code 201 if title is provided and therefore correctly saved in the database", async () => {
+      const response = await request(app).post("/create-todo").send({ title: "Wash the dishes!" });
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual({
+        completed: false,
+        datecompleted: null,
+        datecreated: expect.any(String),
+        id: expect.any(Number),
+        title: "Wash the dishes!",
+      });
+    });
   });
 
-  it("create-todo POST route should return status code 403 if '' is provided as a title", async () => {
-    const response = await request(app).post("/create-todo").send({ title: "" });
-    expect(response.status).toBe(403);
-    expect(response.body).toEqual("A title must be provided in order to save a todo!");
-  });
+  describe.only("update-todo POST", () => {
+    it("should return status code 403 if no id is provided", async () => {
+      const response = await request(app)
+        .patch("/update-todo")
+        .send({ title: "Turn the dishwasher on before leaving home 2morrow" });
 
-  it("create-todo POST route should return status code 201 if title is provided and therefore correctly saved in the database", async () => {
-    const response = await request(app).post("/create-todo").send({ title: "Wash the dishes!" });
-    expect(response.status).toBe(201);
-    expect(response.body).toEqual({
-      completed: false,
-      datecompleted: null,
-      datecreated: expect.any(String),
-      id: expect.any(Number),
-      title: "Wash the dishes!",
+      expect(response.status).toBe(403);
+      expect(response.body).toEqual("An id must be provided if an updated is requested!");
+    });
+
+    it("should return status code 403 if updated title is '', null or undefined", async () => {
+      const response = await request(app).patch("/update-todo").send({ id: 1, title: "" });
+
+      expect(response.status).toBe(403);
+      expect(response.body).toEqual(
+        "A valid title must be provided if a todo's title update is intended"
+      );
+    });
+
+    it("should return status code 201 if 'id' and necessary data to update a todo is provided and these are correctly updated in the database", async () => {
+      const response = await request(app)
+        .patch("/update-todo")
+        .send({ id: 1, title: "Take the dog out for a walk", completed: true });
+
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual({
+        completed: true,
+        dateCompleted: expect.any(String),
+        id: 1,
+        title: "Take the dog out for a walk",
+      });
     });
   });
 });
